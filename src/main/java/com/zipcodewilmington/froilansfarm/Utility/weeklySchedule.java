21692d64d4dcaef694d74e89041316be361fa670
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-20 22:20:40 -0400

    Added console and weeklySchedule classes, stubbed out weeklySchedule and tests.

---
+++ src/main/java/com/zipcodewilmington/froilansfarm/Utility/weeklySchedule.java
@@ -0,0 +1,40 @@
+package com.zipcodewilmington.froilansfarm.Utility;
+
+public class weeklySchedule {
+
+    public void run(){
+
+    }
+
+    public void dailyActivities(){
+
+    }
+
+    public void sundaySchedule(){
+
+    }
+
+    public void mondaySchedule(){
+
+    }
+
+    public void tuesdaySchedule(){
+
+    }
+
+    public void wednesdaySchedule(){
+
+    }
+
+    public void thursdaySchedule(){
+
+    }
+
+    public void fridaySchedule(){
+
+    }
+
+    public void saturdaySchedule(){
+
+    }
+}
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/Utility/weeklyScheduleTest.java
@@ -0,0 +1,40 @@
+package com.zipcodewilmington.froilansfarm.Utility;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class weeklyScheduleTest {
+
+    @Test
+    public void dailyActivities() {
+    }
+
+    @Test
+    public void sundaySchedule() {
+    }
+
+    @Test
+    public void mondaySchedule() {
+    }
+
+    @Test
+    public void tuesdaySchedule() {
+    }
+
+    @Test
+    public void wednesdaySchedule() {
+    }
+
+    @Test
+    public void thursdaySchedule() {
+    }
+
+    @Test
+    public void fridaySchedule() {
+    }
+
+    @Test
+    public void saturdaySchedule() {
+    }
+}
