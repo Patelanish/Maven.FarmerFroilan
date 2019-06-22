b7d4fb041522c6bbaf0e29a8288f4c938919157d
Ajulu Adigwe <47484244+julss16@users.noreply.github.com>
2019-06-22 18:30:40 -0400

    Update FroilanTest.java

--- src/test/java/com/zipcodewilmington/froilansfarm/Person/FroilanTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Person/FroilanTest.java
@@ -8,11 +8,42 @@ public class FroilanTest {
 
     @Test
     public void plant() {
+
+        Crop crop = new Crop() {
+            @Override
+            public int hashCode() {
+                return super.hashCode();
+            }
+        };
+        Integer expected= 1;
+        Froilan froilan = new Froilan();
+        CropRow cropRow = new CropRow();
+
+        ArrayList<Crop> list  = new ArrayList<>();
+
+        list.add(crop);
+
+        froilan.plant(crop);
+
+        Integer actual= cropRow.getCrops().size();
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void eat() {
+        Edible edible= new EdibleEgg();
+
+        //eat(Edible food, Integer qty)
+        Froilan froilan = new Froilan();
+        froilan.eat(edible, 1);
+        boolean expected= true;
+        boolean actual= froilan.hasEaten();
+
+        Assert.assertEquals(expected, actual);
+
     }
+
 
     @Test
     public void makeNoise() {
